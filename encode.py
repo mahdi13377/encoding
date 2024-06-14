bd25b64d0c698276411c1b7ce9d5750d90d6e510
mahdi <147645370+mahdi13377@users.noreply.github.com>
2024-06-14 13:47:07 -0700

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,29 +1,45 @@
-import sys
+unicode_range = 0x10000 
 
-string = ''.join(sys.argv[1:])
+def decode(string):
+    alphabet = ''.join(chr(i) for i in range(unicode_range))
+    parts = [string[i:i+2] for i in range(0, len(string), 2)]
+    if parts[-1].endswith('='):
+        parts[-1] = parts[-1][:-1]
 
-alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    decoded_nodes = []
+    for index, node in enumerate(parts):
+        reversed_node = ""
+        for character in node:
+            if character in alphabet:
+                new_index = (alphabet.index(character) - index) % len(alphabet)
+                reversed_node += alphabet[new_index]
+            else:
+                reversed_node += character
+        decoded_nodes.append(reversed_node)
 
-def nodify(s):
-    parts = [s[i:i+2] for i in range(0, len(s), 2)]
+    decoded_nodes = decoded_nodes[::-1]
+    decoded_string = ''.join(decoded_nodes)
+    return decoded_string
+
+def encode(string):
+    alphabet = ''.join(chr(i) for i in range(unicode_range))
+    parts = [string[i:i+2] for i in range(0, len(string), 2)]
     
     if len(parts[-1]) == 1:
         parts[-1] += '='
     
-    return parts[::-1]
+    nodes = parts[::-1]
 
-nodes = nodify(string)
+    nodes_a = []
 
-nodes_a = []
+    for node_index, node in enumerate(nodes):
+        new_node = ""
+        for character in node:
+            if character in alphabet:
+                new_character = alphabet[(alphabet.index(character) + node_index) % len(alphabet)]
+                new_node += new_character
+            else:
+                new_node += character
+        nodes_a.append(new_node)
 
-for node_index, node in enumerate(nodes):
-    new_node = ""
-    for character in node:
-        if character in alphabet:
-            new_character = alphabet[(alphabet.index(character) + node_index) % len(alphabet)]
-            new_node += new_character
-        else:
-            new_node += character
-    nodes_a.append(new_node)
-
-print(''.join(nodes_a))
+    return ''.join(nodes_a)
